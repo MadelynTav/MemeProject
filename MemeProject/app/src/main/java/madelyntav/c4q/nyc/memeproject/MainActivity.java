a42d85c997d6cc7839cd40dd4b3875f2e02e675b
MadelynTav <Madelyntav@gmail.com>
2015-05-30 17:41:08 -0400

    open camera added by Ray

--- MemeProject/app/src/main/java/madelyntav/c4q/nyc/memeproject/MainActivity.java
+++ MemeProject/app/src/main/java/madelyntav/c4q/nyc/memeproject/MainActivity.java
@@ -1,12 +1,20 @@
 package madelyntav.c4q.nyc.memeproject;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
+    private ImageView mImageView;
+    private Bitmap mImageBitmap;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,26 +22,14 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+//    public void usePic (View v){
+//        Intent intent = new Intent(MainActivity.this, usePic.class);
+//        startActivity(intent);
+//    }
+    public void takePic (View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
