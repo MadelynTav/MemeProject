4e430b48d73936deab5cc3d5d68e8acb728f9e1a
MadelynTav <Madelyntav@gmail.com>
2015-05-30 23:02:58 -0400

    sharing photo method

--- MemeProject/app/src/main/java/madelyntav/c4q/nyc/memeproject/EditPhoto.java
+++ MemeProject/app/src/main/java/madelyntav/c4q/nyc/memeproject/EditPhoto.java
@@ -1,13 +1,20 @@
 package madelyntav.c4q.nyc.memeproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.net.URI;
 
 
 public class EditPhoto extends ActionBarActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,25 +22,18 @@ public class EditPhoto extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_edit_photo, menu);
-        return true;
-    }
+    public void sharePhoto(View view){
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(imageView.getDrawable()!=null) {
+            Uri imageUri = Uri.parse(String.valueOf(imageView.getDrawable()));
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_STREAM, imageUri);
+            intent.setType("image/jpeg");
+            startActivity(intent);
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
+
--- MemeProject/app/src/main/java/madelyntav/c4q/nyc/memeproject/MainActivity.java
+++ MemeProject/app/src/main/java/madelyntav/c4q/nyc/memeproject/MainActivity.java
@@ -22,10 +22,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-//    public void usePic (View v){
-//        Intent intent = new Intent(MainActivity.this, usePic.class);
-//        startActivity(intent);
-//    }
+
     public void takePic (View v) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
