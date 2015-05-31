88b9e7201f02596df83860db19bfabfef314e849
KMaragh <ksmaragh@gmail.com>
2015-05-31 01:24:18 -0400

    Started save picture function

--- MemeProject/app/src/main/java/madelyntav/c4q/nyc/memeproject/EditPhoto.java
+++ MemeProject/app/src/main/java/madelyntav/c4q/nyc/memeproject/EditPhoto.java
@@ -2,15 +2,26 @@ package madelyntav.c4q.nyc.memeproject;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class EditPhoto extends ActionBarActivity {
     private ImageView imageView;
+
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    private File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,31 +30,66 @@ public class EditPhoto extends ActionBarActivity {
         imageView = (ImageView) findViewById(R.id.imageView);
 
         //opens pic in this activity
-        if(getIntent().hasExtra("byteArray")) {
+        if (getIntent().hasExtra("byteArray")) {
             Bitmap b = BitmapFactory.decodeByteArray(getIntent().getByteArrayExtra("byteArray"), 0, getIntent().getByteArrayExtra("byteArray").length);
             imageView.setImageBitmap(b);
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_edit_photo, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    /**
+     * Create a File for saving an image or video
+     */
+    private static File getOutputMediaFile(int type) {
+        //Checks to see if an SDCard is mounted
+        if (Environment.getExternalStorageState() != null) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            //Creates a folder on the external SDCard, under Pictures called 'MemePictures'
+            File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES), "MemePictures");
+
+            // Checks to see if the folder 'MemePictures' was created from the above code.
+            if (!mediaStorageDir.exists()) {
+                if (!mediaStorageDir.mkdirs()) {
+                    Log.d("MemePictures", "failed to create directory");
+                    return null;
+                }
+            }
+
+            // Create a media file name, timeStamp is for appending to file name
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            File mediaFile;
+            if (type == MEDIA_TYPE_IMAGE) {
+                mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                        "IMG_" + timeStamp + ".jpg");
+            }
+            else {
+                return null;
+            }
+
+            return mediaFile;
+        }
+        else {
+            return null;
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+    //Method for saving a meme
+    public void savePicture() {
+        file = getOutputMediaFile(MEDIA_TYPE_IMAGE); // create a file to save the image
+        Bitmap image = ((BitmapDrawable)imageView.getDrawable()).getBitmap(); //gets the bitmap from the current imageview
+
+        try {
+            FileOutputStream fOut = new FileOutputStream(file);
+
+            image.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+            fOut.flush();
+            fOut.close();
+        }
+        catch(Exception e){
+            Toast.makeText(this,"Could not save picture",Toast.LENGTH_LONG);
+        }
+    }
+
 }
