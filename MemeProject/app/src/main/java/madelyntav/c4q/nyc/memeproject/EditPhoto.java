2b0c9252067f38ddf3de2b28fc260c06ec91ae25
KMaragh <ksmaragh@gmail.com>
2015-06-02 13:25:36 -0400

    Finished storeImage function to Camera Roll

--- MemeProject/app/src/main/java/madelyntav/c4q/nyc/memeproject/EditPhoto.java
+++ MemeProject/app/src/main/java/madelyntav/c4q/nyc/memeproject/EditPhoto.java
@@ -1,9 +1,14 @@
 package madelyntav.c4q.nyc.memeproject;
 
-import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
@@ -13,7 +18,6 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -26,77 +30,89 @@ public class EditPhoto extends ActionBarActivity {
 
     private File file;
     private String TAG = "GallerySaving";
+    RelativeLayout memeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_photo);
         imageView = (ImageView) findViewById(R.id.mImageView);
+        memeLayout = (RelativeLayout)findViewById(R.id.meme);
 
         //opens pic in this activity
         if (getIntent().hasExtra("byteArray")) {
             Bitmap b = BitmapFactory.decodeByteArray(getIntent().getByteArrayExtra("byteArray"), 0, getIntent().getByteArrayExtra("byteArray").length);
             imageView.setImageBitmap(b);
+
         }
 
     }
 
+    //onClick method for the save button. Calls other methods to create the save image function
     public void storeImage(View v) {
-        //converts the current imageview into a bitmap for storage purposes
-        //Bitmap image = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+        Bitmap image = getBitmapFromView(memeLayout);
+        File pictureFile = createImageFile();
+        addImageToFile(image, pictureFile);
 
-        //RelativeLayout rel = (RelativeLayout) findViewById(R.id.meme);
-        Bitmap image = createBitmap();
+    }
+    /**
+     * Create a File for saving an image or video
+     * Handles file name, and where to store it
+     */
+    private File createImageFile() {
+        String timeStamp = new SimpleDateFormat("MMddyyyy_HHmmss").format(new Date());
+        String imageName = "MEME_" + timeStamp + ".jpg";
 
-        //Calls the getOutputMediaFile method to create the file the image will be stored in
-        File pictureFile = getOutputMediaFile();
-        if (pictureFile == null) {
-            Log.d(TAG,
-                    "Error creating media file, check storage permissions: ");// e.getMessage());
-            return;
-        }
-        try {
-            FileOutputStream fos = new FileOutputStream(pictureFile);
-            image.compress(Bitmap.CompressFormat.PNG, 90, fos);
-            fos.close();
-            Toast.makeText(this, "Saved image to camera", Toast.LENGTH_LONG).show();
-        } catch (FileNotFoundException e) {
-            Log.d(TAG, "File not found: " + e.getMessage());
-        } catch (IOException e) {
-            Log.d(TAG, "Error accessing file: " + e.getMessage());
-        }
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File imageFile = new File (path, imageName);
+        Log.d("Path: ", path.getPath());
+        return imageFile;
+
     }
 
-    /** Create a File for saving an image or video
-     * Handles file name, and where to store it */
-    private  File getOutputMediaFile(){
+    private void addImageToFile(Bitmap image, File file){
+        FileOutputStream fos = null;
+        //Tries to add the bitmap image to the file
+        try {
+            fos = new FileOutputStream(file);
+            image.compress(Bitmap.CompressFormat.PNG, 90, fos);
 
-        // path to /data/data/yourapp/app_data/imageDir
-        File mediaStorageDir = getApplicationContext().getDir("imageDir", Context.MODE_PRIVATE);
-        // Create imageDir
-        File mypath=new File(mediaStorageDir, "profile.jpg");
-        // Create the storage directory if it does not exist
-        if (! mediaStorageDir.exists()){
-            if (! mediaStorageDir.mkdirs()){
-                return null;
+            Toast.makeText(this, "Saved image to camera pictures", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Log.d(TAG,"trying to compress image did not work" + e.getMessage());
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close(); //Closes the fileoutput stream
+                    //Scans the image file that was just created so user can immediately see it in Pictures
+                    try{
+                        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                        Uri uri = Uri.fromFile(file);
+                        mediaScanIntent.setData(uri);
+                        sendBroadcast(mediaScanIntent);
+                    }catch(Exception e){
+                        Log.d("scanIntent", "Failed: ");
+                    }
+                }
+            } catch (IOException e) {
+                Log.d(TAG, "fos did not close " + e.getMessage());
             }
         }
-        // Create a media file name, format is
-        String timeStamp = new SimpleDateFormat("ddMMyyyy_HHmm").format(new Date());
-        File mediaFile;
-        String mImageName="MEME_"+ timeStamp +".jpg";
-        mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
-        return mediaFile;
+
     }
 
-    public Bitmap createBitmap(){
-        RelativeLayout view = (RelativeLayout)findViewById(R.id.meme);
-        view.setDrawingCacheEnabled(true);
-        view.buildDrawingCache();
-        Bitmap bm = view.getDrawingCache();
-        Toast.makeText(this,"created Bitmap method worked", Toast.LENGTH_SHORT).show();
-        return bm;
+    //Takes the current view and creates a bitmap representing that view.
+    public Bitmap getBitmapFromView(View view) {
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+
+        Drawable bgDrawable = view.getBackground();
+        if (bgDrawable != null)
+            bgDrawable.draw(canvas);
+        else
+            canvas.drawColor(Color.WHITE);
+        view.draw(canvas);
+        return returnedBitmap;
     }
-
-
 }
