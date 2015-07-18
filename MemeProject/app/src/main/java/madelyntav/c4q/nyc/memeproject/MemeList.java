8a65b8beab6603a2eea0584712ef77ca1b229c02
AnthonyFermin <AnthonyFerminAFC@gmail.com>
2015-07-17 20:00:26 -0400

    Added Meme Templates from imgflip.com & store link info into an SQLite DB

--- MemeProject/app/src/main/java/madelyntav/c4q/nyc/memeproject/MemeList.java
+++ MemeProject/app/src/main/java/madelyntav/c4q/nyc/memeproject/MemeList.java
@@ -1,27 +1,40 @@
 package madelyntav.c4q.nyc.memeproject;
 
 import android.app.Activity;
-import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
+
+import madelyntav.c4q.nyc.memeproject.Meme_API.Meme;
+import madelyntav.c4q.nyc.memeproject.Meme_API.MemeTemplate;
+import madelyntav.c4q.nyc.memeproject.Meme_API.MemeTemplateAPI;
+import madelyntav.c4q.nyc.memeproject.Meme_DB.MySQLiteOpenHelper;
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 /**
  * Created by kadeemmaragh on 6/5/15.
  */
 public class MemeList extends Activity {
 
+    private static final String DB_FULL_PATH = "//data/data/madelyntav.c4q.nyc.memeproject/databases/myDb";
     ListView listView;
-    HashMap<Integer, String> memePairs;
     private Uri uri;
+    List<Meme> memeList;
+    List<String> imageUrls;
+    List<String> imageTitles;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,60 +43,115 @@ public class MemeList extends Activity {
 
         listView = (ListView) findViewById(R.id.listView);
 
-        memePairs = new HashMap<>();
-        memePairs.put(R.drawable.actual_advice_mallard, "Actual Advice Mallard");
-        memePairs.put(R.drawable.but_thats_none_of_my_business, "But That's None Of My Business");
-        memePairs.put(R.drawable.creepy_condescending_wonka, "Creepy Condescending Wonka");
-        memePairs.put(R.drawable.futurama_fry, "Skeptical Fry");
-        memePairs.put(R.drawable.good_guy_greg, "Good Guy Greg");
-        memePairs.put(R.drawable.liam_neeson_taken, "Liam Neeson Taken");
-        memePairs.put(R.drawable.one_does_not_simply, "One Does Not Simply");
-        memePairs.put(R.drawable.scumbag_steve, "Scumbag Steve");
-        memePairs.put(R.drawable.shut_up_and_take_my_money_fry, "Shut Up And Take My Money");
-        memePairs.put(R.drawable.ten_guy, "Ten Guy");
-        memePairs.put(R.drawable.the_most_interesting_man_in_the_world, "The Most Interesting Man In The World");
-        memePairs.put(R.drawable.third_world_skeptical_kid, "Third World Skeptical Kid");
-        memePairs.put(R.drawable.unhelpful_high_school_teacher, "Unhelpful High School Teacher");
-        memePairs.put(R.drawable.yao_ming, "Yao Ming");
-        memePairs.put(R.drawable.you_the_real_mvp, "You The Real MVP");
+        if(dataBaseExists()){
 
+            memeList = loadData();
 
-        final ArrayList<Integer> memeImages = new ArrayList<Integer>();
-        ArrayList<String> memeNames = new ArrayList<String>();
-        addItemsToArrays(memeImages, memeNames);
+            imageUrls = new ArrayList<String>();
+            imageTitles = new ArrayList<String>();
+            for(Meme meme : memeList){
+                imageUrls.add(meme.getUrl());
+                imageTitles.add(meme.getName());
+            }
 
-        CustomArrayAdapter memeAdapter = new CustomArrayAdapter(getApplicationContext(), memeNames, memeImages);
+            listView.setAdapter(new CustomArrayAdapter(getApplicationContext(), imageTitles, imageUrls, true));
+        }else{
 
-        listView.setAdapter(memeAdapter);
+            retrofitGetMemeTemplates();
+        }
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(MemeList.this, EditPhoto.class);
 
-                uri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getResources().getResourcePackageName(memeImages.get(position)) + '/' + getResources().getResourceTypeName(memeImages.get(position)) + '/' + getResources().getResourceEntryName(memeImages.get(position)));
+                //uri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getResources().getResourcePackageName(memeImages.get(position)) + '/' + getResources().getResourceTypeName(memeImages.get(position)) + '/' + getResources().getResourceEntryName(memeImages.get(position)));
 
                 intent.putExtra("image", uri);
                 startActivity(intent);
                 Toast.makeText(getApplicationContext(), "Please select VANILLA or DEMO layout to begin", Toast.LENGTH_SHORT).show();
 
-
             }
         });
 
+    }
 
+    private List<Meme> loadData() {
 
+        MySQLiteOpenHelper db = MySQLiteOpenHelper.getInstance(this);
+        return db.loadData();
+    }
+
+    private void insertData(Meme meme) {
+        MySQLiteOpenHelper db = MySQLiteOpenHelper.getInstance(this);
+        db.insertRow(meme);
+    }
+
+    private void retrofitGetMemeTemplates(){
+
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("https://api.imgflip.com")
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .build();
+
+        MemeTemplateAPI memeTemplateAPI = restAdapter.create(MemeTemplateAPI.class);
+
+        memeTemplateAPI.getTemplates(new Callback<MemeTemplate>() {
+            @Override
+            public void success(MemeTemplate memeTemplate, Response response) {
+                memeList = memeTemplate.getData().getMemes();
+                imageUrls = new ArrayList<String>();
+                imageTitles = new ArrayList<String>();
+                for(Meme meme : memeList){
+                    imageUrls.add(meme.getUrl());
+                    imageTitles.add(meme.getName());
+                }
+
+                loadDBSetAdapter();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                (Toast.makeText(MemeList.this,"Couldn't load popular Memes",Toast.LENGTH_LONG)).show();
+            }
+        });
 
     }
 
-    public void addItemsToArrays(ArrayList<Integer> images, ArrayList<String> titles) {
-        int position = 0;
-        for (Integer image : memePairs.keySet()) {
+    private void loadDBSetAdapter(){
 
-            images.add(position, image);
-            titles.add(position, memePairs.get(image));
-            position++;
+        (new AsyncTask<Void, Void, List<Meme>>() {
+            @Override
+            protected List<Meme> doInBackground(Void... voids) {
+
+                for(Meme meme : memeList) {
+                    insertData(meme);
+                }
+
+                return loadData();
+            }
+
+            @Override
+            protected void onPostExecute(List<Meme> memes) {
+                super.onPostExecute(memes);
+
+                listView.setAdapter(new CustomArrayAdapter(getApplicationContext(), imageTitles, imageUrls, true));
+            }
+        }).execute();
+
+    }
+
+    private boolean dataBaseExists() {
+        SQLiteDatabase checkDB = null;
+        try {
+            checkDB = SQLiteDatabase.openDatabase(DB_FULL_PATH, null,
+                    SQLiteDatabase.OPEN_READONLY);
+            checkDB.close();
+        } catch (SQLiteException e) {
+            // database doesn't exist yet.
         }
+        return checkDB != null;
     }
+
 
 }
